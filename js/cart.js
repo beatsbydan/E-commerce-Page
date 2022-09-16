15e3e2f464d1de2a75478659ee715b87e8ba888f
beatsbydan <danielonyeachonam2020@gmail.com>
2022-09-16 19:39:01 +0100

    update

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,3 @@
-
-
 //cart display
 const cartBtn = document.querySelector(".cart-btn")
 cartBtn.addEventListener("click", () => {
@@ -18,29 +16,50 @@ cartBtn.addEventListener("click", () => {
 const addBtn = document.querySelector(".add")
 const subtractBtn = document.querySelector(".subtract")
 const toCartBtn = document.querySelector(".to-cart")
-const deleteBtn = document.querySelector(".delete")
 let content = document.querySelector(".content")
 let dynamicNumber = document.querySelector(".dynamic--number")
-let resultVal = document.querySelector(".value")
 let notification = document.querySelector(".notificator")
-let notificator = document.querySelector(".notificator span")
-let operand = document.querySelector(".operand")
 
-//storing in the local storage
+//displaying the empty cart by default
+
+content.innerHTML = `<p class="default--text">Your cart is empty.</p>`
+
+
+//storing the cart in the local storage
 //let cart = JSON.parse(localStorage.getItem("item"))
-// localStorage.setItem("item", JSON.stringify(cart))
+
+
 
 //adding and subtracting items
-let quantity = parseFloat(dynamicNumber.innerHTML)
+let quantity = 0
 addBtn.addEventListener("click", () => {
     quantity++;
     dynamicNumber.innerHTML = quantity.toString()
-    notificator.innerHTML = dynamicNumber.innerHTML
-    operand.innerHTML = dynamicNumber.innerHTML
-    let discountPrice = 125
-    let result;
-    result = discountPrice * parseFloat(operand.innerHTML)
-    resultVal.innerHTML = result.toString()
+    //adding to the cart
+    toCartBtn.addEventListener("click", () => {
+        // if(!cart){
+        //     cart = []
+        // }
+        notification.setAttribute("data-visible", "true")
+        notification.firstElementChild.innerHTML = quantity.toString()
+        dynamicNumber.innerHTML = 0
+        //calculating and displaying the cost
+        let fixedPrice = 125
+        let result = fixedPrice * quantity
+        let price = result.toString()
+        content.innerHTML = `<div class="item">
+                                <div class="details">
+                                    <img class="selected" src="./images/image-product-1-thumbnail.jpg" alt="">
+                                    <div class="description">
+                                        <p>Fall Limited Edition Sneakers</p>
+                                        <p class="price">$125.00 x <span class="operand">${quantity}</span><span class="result"> $<span class="value">${price}</span></span></p>
+                                    </div>
+                                </div>
+                                <img class="delete" onclick = "deleteItem()" src="./images/icon-delete.svg" alt="">
+                            </div>
+                            <button class="checkout">Checkout</button> `
+        
+        })
 })
 subtractBtn.addEventListener("click", () => {
     quantity --;
@@ -48,27 +67,13 @@ subtractBtn.addEventListener("click", () => {
         quantity = 0
     }
     dynamicNumber.innerHTML = quantity.toString()
-    notificator.innerHTML = dynamicNumber.innerHTML
-    operand.innerHTML = dynamicNumber.innerHTML
-    let discountPrice = 125
-    let result;
-    result = discountPrice * parseFloat(operand.innerHTML)
-    resultVal.innerHTML = result.toString()
 })
 
-//calculating and displaying the cost
-
-
-//adding to the cart
-
-toCartBtn.addEventListener("click", () => {
-    notification.setAttribute("data-visible", "true")
-})
 //clear the cart
-deleteBtn.addEventListener("click", () => {
+function deleteItem(){
     content.innerHTML = `<p class="default--text">Your cart is empty.</p>`
     notification.setAttribute("data-visible", "false")
-})
-
-
-
+    notification.firstElementChild.innerHTML = null
+    //localStorage.setItem("item", JSON.stringify(cart))
+}
+    
